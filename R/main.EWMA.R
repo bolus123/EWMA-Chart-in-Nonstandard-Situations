dc9be32789f9f8f85bd77026b69dfc3e5d4e63f5
Yuhui Yao <bolus123@users.noreply.github.com>
2018-06-26 13:52:42 -0500

    update

--- R/main.EWMA.R
+++ R/main.EWMA.R
@@ -51,7 +51,7 @@ EWMA.CARL.DP.integral <- function(xmin, xmax, ymin, ymax, L, lambda, mm, ss, rel
 
 }
 
-EWMA.get.cc <- function(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
+EWMA.get.cc.DP <- function(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
 	ymin = 0, ymax = 1, lambda = 0.2, mm = 100, ss = Inf, reltol = 1e-6){
 
 		root.finding <- function(ARL0, xmin, xmax, ymin, ymax, L, lambda, mm, ss, reltol){
@@ -65,25 +65,30 @@ EWMA.get.cc <- function(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1,
 	
 }
 
+#plot.vec <- rep(NA, 6)
+#
+#lambda <- 0.8
+#
+#plot.vec[1] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 2.57), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 10, ss = Inf)
+#	
+#plot.vec[2] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 3), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 20, ss = Inf)
+#	
+#plot.vec[3] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 50, ss = Inf)
+#	
+#plot.vec[4] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 100, ss = Inf)	
+#	
+#plot.vec[5] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 200, ss = Inf)	
+#	
+#plot.vec[6] <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
+#	ymin = 0, ymax = 1, lambda = lambda, mm = 300, ss = Inf)	
 
-aa <- EWMA.get.cc(ARL0 = 370, interval = c(1, 2.57), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 10, ss = Inf)
-	
-bb <- EWMA.get.cc(ARL0 = 370, interval = c(1, 3), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 20, ss = Inf)
-	
-cc <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 50, ss = Inf)
-	
-dd <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 100, ss = Inf)	
-	
-ee <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 200, ss = Inf)	
-	
-ff <- EWMA.get.cc(ARL0 = 370, interval = c(1, 4), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = 0.2, mm = 300, ss = Inf)	
 
+	
 ####################################################################################################################################################
 
 EWMA.ARLin.performance <- function(L, lambda, mm, ss, sim = 10000){
@@ -104,3 +109,90 @@ EWMA.ARLin.performance <- function(L, lambda, mm, ss, sim = 10000){
 EWMA.ARLin.performance(L = 2.963359, lambda = 0.2, mm = 20, ss = Inf, sim = 1000000)
 
 ####################################################################################################################################################
+
+EWMA.CARL.MC.Q <- function(U, V, L, lambda, mm, ss, tt) {
+
+	nn <- 2 * tt + 1
+
+	Q <- matrix(NA, ncol = nn, nrow = nn)
+	
+	for (l in 1:nn){
+	
+		ll <- l - (tt + 1)
+	
+		for (k in 1:nn){
+		
+			kk <- k - (tt + 1)
+		
+			Q[l, k] <- pnorm(qnorm(U) / sqrt(mm) + (2 * ll - (1 - lambda) * 2 * kk + 1) / nn / lambda * 
+				(L * sqrt(lambda / (2 - lambda) * (1 - (1 - lambda)^(2 * ss))) * sqrt(qchisq(V, mm - 1)) / c4.f(mm - 1) / sqrt(mm - 1) )) - 
+				pnorm(qnorm(U) / sqrt(mm) + (2 * ll - (1 - lambda) * 2 * kk - 1) / nn / lambda * 
+				(L * sqrt(lambda / (2 - lambda) * (1 - (1 - lambda)^(2 * ss))) * sqrt(qchisq(V, mm - 1)) / c4.f(mm - 1) / sqrt(mm - 1) ))
+		
+		}
+	
+	}
+	
+	Q
+	
+}
+
+EWMA.CARL.MC.xi <- function(tt, mid = tt + 1) {
+
+	nn <- 2 * tt + 1
+	
+	xi.vec <- rep(0, nn)
+	
+	xi.vec[mid] <- 1
+
+	xi.vec
+
+}
+
+EWMA.CARL.MC.integrand <- function(U, V, L, lambda, mm, ss, tt){
+
+	QQ <- EWMA.CARL.MC.Q(U, V, L, lambda, mm, ss, tt)
+	xi.vec <- EWMA.CARL.MC.xi(tt)
+	xi.vec <- matrix(xi.vec, nrow = 1, ncol = 2 * tt + 1)
+	I.matrix <- diag(2 * tt + 1)
+	
+	one.vec <- matrix(1, ncol = 1, nrow = 2 * tt + 1)
+	
+	xi.vec %*% solve(I.matrix - QQ) %*% one.vec
+
+}
+
+#debug(EWMA.CARL.MC)
+
+#EWMA.CARL.MC(0.5, 0.5, 3, 0.2, 100, Inf, 5)
+
+EWMA.CARL.MC.integral <- function(xmin, xmax, ymin, ymax, L, lambda, mm, ss, tt = 3, reltol = 1e-6){
+
+	#cat(m, '\n')
+
+	integral2(EWMA.CARL.MC.integrand, xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, 
+		singular = TRUE, vectorized = FALSE, L = L, lambda = lambda, mm = mm, ss = ss, tt = tt, reltol = reltol)$Q
+
+}
+
+
+
+#debug(EWMA.CARL.MC.Q)
+
+EWMA.get.cc.MC <- function(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
+	ymin = 0, ymax = 1, lambda = 0.2, mm = 100, ss = Inf, tt = 3, reltol = 1e-6){
+
+		root.finding <- function(ARL0, xmin, xmax, ymin, ymax, L, lambda, mm, ss, tt, reltol){
+		
+			ARL0 - EWMA.CARL.MC.integral(xmin, xmax, ymin, ymax, L, lambda, mm, ss, tt, reltol)
+		
+		}
+	
+		uniroot(root.finding, interval = interval, ARL0 = ARL0, xmin = xmin, xmax = xmax,
+			ymin = ymin, ymax = ymax, lambda = lambda, mm = mm, ss = ss, tt = tt, reltol = reltol)$root
+	
+}
+
+EWMA.get.cc.MC(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
+	ymin = 0, ymax = 1, lambda = 0.2, mm = 100, ss = Inf, tt = 3, reltol = 1e-6)
+
