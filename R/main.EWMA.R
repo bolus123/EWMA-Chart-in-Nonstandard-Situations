d5cf685da2cd876eddc9568b71b7b2cd67b11399
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-27 14:58:04 -0600

    update

--- R/main.EWMA.R
+++ R/main.EWMA.R
@@ -1,10 +1,10 @@
 source('https://raw.githubusercontent.com/bolus123/EWMA-Chart-in-Nonstandard-Situations/master/R/head.R')
 
-EWMA.get.cc.MC(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = in.vec[X, 1], mm = in.vec[X, 3], ss = Inf, tt = 100, reltol = 1e-6, tol = 1e-6)
+a <- EWMA.get.cc.MC(ARL0 = 370, interval = c(1, 5), xmin = 0, xmax = 1, 
+	ymin = 0, ymax = 1, lambda = 0.1, mm = 20, ss = Inf, tt = 50, reltol = 1e-6, tol = 1e-6)
     
-EWMA.get.cc.Conditional.MC(p0 = in.vec[X, 1], interval = c(1, 6.5), xmin = 0, xmax = 1, 
-	ymin = 0, ymax = 1, lambda = in.vec[X, 2], eplison = in.vec[X, 3], ARL0 = in.vec[X, 4], mm = in.vec[X, 5], ss = Inf, tt = 100, reltol = 1e-6, tol = 1e-6)   
+b <- EWMA.get.cc.Conditional.MC(p0 = 0.1, interval = c(1, 6.5), xmin = 0, xmax = 1, 
+	ymin = 0, ymax = 1, lambda = 0.1, eplison = 0.1, ARL0 = 370, mm = 20, ss = Inf, tt = 50, reltol = 1e-6, tol = 1e-6)   
 
 ######################################################################################################
 
